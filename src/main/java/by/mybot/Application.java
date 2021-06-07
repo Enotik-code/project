26480e5a64bfaa03ab6329a223db760f487c218c
Anton <fvantop@gmail.com>
2021-06-07 22:29:40 +0300

    add data.sql

--- src/main/java/by/mybot/Application.java
+++ src/main/java/by/mybot/Application.java
@@ -1,7 +1,5 @@
 package by.mybot;
 
-import by.mybot.payment.repository.OperationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
