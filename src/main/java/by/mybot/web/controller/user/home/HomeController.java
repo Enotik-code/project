ea4e572969a66a26540be79c772662a17edc3b72
akupreichik <anton.kupreichik@ontravelsolutions.com>
2021-06-23 13:30:26 +0300

    add some html files

--- src/main/java/by/mybot/web/controller/user/home/HomeController.java
+++ src/main/java/by/mybot/web/controller/user/home/HomeController.java
@@ -2,28 +2,17 @@ package by.mybot.web.controller.user.home;
 
 import by.mybot.service.locations.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class HomeController {
 
-    @Autowired
-    private LocationService locationService;
-
-    @GetMapping(value = "/air")
+    @GetMapping(value = "/home")
     public ModelAndView getHomePage(){
-        ModelAndView modelAndView = new ModelAndView("/home");
+        ModelAndView modelAndView = new ModelAndView("/home.html");
         return modelAndView;
     }
 
-    /*@RequestMapping(value = "/processing", method = RequestMethod.GET)
-    public void processData (HttpServletRequest request) {
-        System.out.println(request.getRemoteAddr());
-
-    }*/
 }
