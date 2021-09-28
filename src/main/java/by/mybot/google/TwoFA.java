bf28167ee2311831f550a7c84482fc01f73ac2cd
Enotik-code <fvantop@gmail.com>
2021-09-28 22:53:12 +0300

    add google 2fa

---
+++ src/main/java/by/mybot/google/TwoFA.java
@@ -0,0 +1,33 @@
+package by.mybot.google;
+
+import by.cryptic.google.auth.TwoFAUtils;
+import com.google.zxing.WriterException;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@Component
+public class TwoFA {
+
+    public static void main(String[] args) throws IOException, WriterException {
+        String secretKey = "KMMC7VFMM3YNN5KX6GL4W5TPHQ47RN3T";
+        String email = "fvantop@gmail.com";
+        String companyName = "BitCash";
+        String barCodeUrl = TwoFAUtils.getGoogleAuthenticatorBarCode(secretKey, email, companyName);
+        System.out.println(barCodeUrl);
+        TwoFAUtils.createQRCode(barCodeUrl, "QRCode.png", 400, 400);
+
+        Scanner scanner = new Scanner(System.in);
+        String code = scanner.nextLine();
+        if (code.equals(TwoFAUtils.getTOTPCode(secretKey))) {
+            System.out.println("Logged in successfully");
+        } else {
+            System.out.println("Invalid 2FA Code");
+        }
+    }
+
+    public void execute(){
+
+    }
+}
---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+#insert operations
+insert into project.operations values (1, 'Refill'),(2, 'Withdraw funds'), (3, 'Operation failure'), (4, 'Transfer');
+
+#insert some users
